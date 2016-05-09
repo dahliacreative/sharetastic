295d14a3a22dfc0ebbca30a2b35b51dbc7d36c7d
Simon Sturgess <simon@dahliacreative.com>
2016-05-09 16:41:29 +0100

    Adding sprite to dist

--- grunt/copy.js
+++ grunt/copy.js
@@ -3,7 +3,8 @@ module.exports = {
     files: [
       {expand: false, src: ['src/stylesheets/sharetastic.sass'], dest: 'dist/sharetastic.sass'},
       {expand: false, src: ['dist/sharetastic.js'], dest: 'tests/sharetastic.js'},
-      {expand: false, src: ['src/images/sharetastic.svg'], dest: 'tests/sharetastic.svg'}
+      {expand: false, src: ['src/images/sharetastic.svg'], dest: 'tests/sharetastic.svg'},
+      {expand: false, src: ['src/images/sharetastic.svg'], dest: 'dist/sharetastic.svg'}
     ]
   }
 }
