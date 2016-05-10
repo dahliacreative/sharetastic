52e224c6c5d4bbb53610e4ebd697d9ea384a6731
Simon Sturgess <simon@dahliacreative.com>
2016-05-10 15:42:07 +0100

    Adding popup

--- app/javascript/sharetastic.js
+++ app/javascript/sharetastic.js
@@ -82,22 +82,31 @@
   }
 };
 
+
 // --------------------------------------------------------------------------
 // Build the DOM
 // --------------------------------------------------------------------------
 Sharetastic.prototype.build = function() {
   for(var key in this.options.feeds) {
     if(this.options.feeds[key]) {
-      var link = $('<a/>');
+      var link = $('<a/>'),
+          self = this;
       link
         .addClass(this.feeds[key].class)
         .attr('href', this.feeds[key].href)
         .attr('target', this.feeds[key].target)
         .html(this.feeds[key].icon);
       this.el.append(link);
+      if(key != 'email') {
+        link.on('click', function() {
+          self.popup($(this).attr('href'), 500, 300);
+          return false;
+        });
+      }
     }
   }
 };
+
 
 // --------------------------------------------------------------------------
 // Get Meta Content
@@ -111,4 +120,14 @@ Sharetastic.prototype.getMetaContent = function(propName) {
     }
   }
   return "";
+};
+
+
+// --------------------------------------------------------------------------
+// Pop up Window
+// --------------------------------------------------------------------------
+Sharetastic.prototype.popup = function(url, width, height) {
+  var left = (screen.width / 2) - (width / 2),
+      top = (screen.height / 2) - (height / 2);
+  window.open(url, "", "menubar=no,toolbar=no,resizable=yes,scrollbars=yes,width=" + width + ",height=" + height + ",top=" + top + ",left=" + left);
 };
