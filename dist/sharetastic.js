a7cfd533f578e8a624a6e5a5de7aefcea5f910cb
dahliacreative <simon@dahliacreative.com>
2017-05-23 11:37:04 +0100

    Using jquery extend for deep merge of options

--- dist/sharetastic.js
+++ dist/sharetastic.js
@@ -1,8 +1,6 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -166,7 +164,7 @@ var Sharetastic = function () {
                 }
             };
 
-            var exportOptions = _extends(defaults, options);
+            var exportOptions = $.extend(true, defaults, options);
 
             if (this.element.hasAttribute('data-tweet')) {
                 exportOptions.services.twitter.href = 'http://twitter.com/home?status=' + encodeURIComponent(this.element.getAttribute('data-tweet'));
@@ -211,6 +209,7 @@ var Sharetastic = function () {
         value: function sort() {
             var _this2 = this;
 
+            console.log(this.options);
             this.serviceOrder = [];
             Object.keys(this.options.services).map(function (k, i) {
                 Object.keys(_this2.options.services).map(function (key) {
